22d7c875b50b9d31682bf4f8228ee2220ed36c5a
ManatoKawa <2004240008km@edu.oca.ac.jp>
2023-02-18 16:42:44 +0900

    ffff

---
+++ test_default_export.js
@@ -0,0 +1,4 @@
+export let value = 0; // 通常のエクスポート
+export default function increment(){ // defaultエクスポート
+return ++value;
+}
---
+++ test_default_import.js
@@ -0,0 +1,3 @@
+import increment, { value } from "./test_default_export.js"; // default
+increment();
+console.log(value);
